a3770137ac5c4e0837dee25e51f4d0612330c1ae
Nokii-Code <felix.potratz2003@gmail.com>
2023-04-24 12:14:18 +0200

    Added Maven for executeablility.

--- src/main/java/com/example/blackjack/Card.java
+++ src/main/java/com/example/blackjack/Card.java
@@ -1,2 +1,65 @@
-package com.example.blackjack;public class Card {
+package com.example.blackjack;
+
+public class Card {
+
+    private final String card;
+
+    private final int cardValue;
+
+    private static String cardTail;
+    private static String cardHead;
+    private static String cardBody;
+
+    private final static String[] CARDSHEAD = {"K", "P", "C", "H"};
+
+    private final static String[] CARDSTAIL = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+    public Card() {
+        cardTail = CARDSTAIL[(int)(Math.random() * CARDSTAIL.length)];
+        cardHead = CARDSHEAD[(int)(Math.random() * CARDSHEAD.length)];
+        cardBody = cardHead + " " + cardTail;
+        int cardValueT;
+        cardBody = create(cardBody);
+        if (cardTail.equals("J") ||
+                cardTail.equals("Q") ||
+                    cardTail.equals("K")) {
+            cardValueT = 10;
+        } else if (cardTail.equals("A") && Controller.cardsCountedValue + 11 > 21) {
+            cardValueT = 1;
+        } else if (cardTail.equals("A") && Controller.cardsCountedValue + 11 < 21) {
+            cardValueT = 11;
+        } else {
+            cardValueT = Integer.parseInt(cardTail);
+        }
+        this.card = cardBody;
+        this.cardValue = cardValueT;
+    }
+
+    public static String create(String s) {
+        for (Card c : Controller.cards) {
+            if (c.getName().equals(s)) {
+                cardTail = CARDSTAIL[(int)(Math.random() * CARDSTAIL.length)];
+                cardHead = CARDSHEAD[(int)(Math.random() * CARDSHEAD.length)];
+                cardBody = cardHead + " " + cardTail;
+                create(cardBody);
+            }
+        }
+        for (Card d : Controller.cardsDealer) {
+            if (d.getName().equals(s)) {
+                cardTail = CARDSTAIL[(int)(Math.random() * CARDSTAIL.length)];
+                cardHead = CARDSHEAD[(int)(Math.random() * CARDSHEAD.length)];
+                cardBody = cardHead + " " + cardTail;
+                create(cardBody);
+            }
+        }
+        return cardBody;
+    }
+
+    public int getValue() {
+        return this.cardValue;
+    }
+
+    public String getName() {
+        return this.card;
+    }
 }
--- src/main/java/com/example/blackjack/Controller.java
+++ src/main/java/com/example/blackjack/Controller.java
@@ -2,20 +2,33 @@ package com.example.blackjack;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.util.ArrayList;
 
-public class HelloController {
+import static javafx.scene.paint.Color.*;
 
-    private ArrayList<Card> cards = new ArrayList<>();
+public class Controller {
+
+    public static ArrayList<Card> cards = new ArrayList<>();
+
+    public static ArrayList<Card> cardsDealer = new ArrayList<>();
+
+    public static int multiplier;
+
+    public static int superWin;
 
     public static int cardsCountedValue;
 
     public static int cardsCountedDealer;
 
-    public static int guthaben;
+    public static int guthaben = 2000;
+
+    public static boolean played = false;
 
     @FXML
     private Label onWinLabel;
@@ -33,10 +46,10 @@ public class HelloController {
     private Label countedCardsBank;
 
     @FXML
-    private TableColumn cardDeck;
+    private Label cardDeck;
 
     @FXML
-    private TableView cardOverview;
+    private Label cardDeckDealer;
 
     @FXML
     private TextField betValue;
@@ -47,18 +60,81 @@ public class HelloController {
     @FXML
     private Button betting;
 
-    final ObservableList<String> data = FXCollections.observableArrayList(
-            new Card().getName()
-    );
+    @FXML
+    private Button stopping;
+
+    @FXML
+    private Label winning;
+
+    @FXML
+    private Label profit;
+
+    @FXML
+    private Label mult;
+
+    @FXML
+    private Button restartButton;
+
+    public static int gewinn;
+
+    public static boolean cheat = false;
 
     public void fail() {
-        guthabenLabel.setText("Guthaben: " + guthaben);
+        guthabenLabel.setText("Guthaben: " + guthaben + "€");
 
+        profit.setVisible(true);
+        profit.setTextFill(RED);
+        profit.setText("[-" + gewinn / multiplier + "]");
+
+        countedCardsBank.setText("Bank: " + cardsCountedDealer);
+
+        String update = "";
+        for (Card c : cardsDealer) {
+            update += c.getName() + "; ";
+        }
+        cardDeckDealer.setText("[ " + update.substring(0, update.length() - 2) + " ]");
+
+        gewinn = 0;
+
+        if (guthaben == 0) {
+            restartButton.setVisible(true);
+            newCardButton.setDisable(true);
+            stopping.setDisable(true);
+            return;
+        }
+
+        restart();
     }
 
     public void win() {
-        guthabenLabel.setText("Guthaben: " + guthaben);
+        guthaben += gewinn;
+        guthabenLabel.setText("Guthaben: " + guthaben + "€");
 
+        profit.setVisible(true);
+        profit.setTextFill(GREEN);
+        profit.setText("[+" + gewinn + "]");
+
+        countedCardsBank.setText("Bank: " + cardsCountedDealer);
+
+        String update = "";
+            for (Card c : cardsDealer) {
+                update += c.getName() + "; ";
+            }
+        cardDeckDealer.setText("[ " + update.substring(0, update.length() - 2) + " ]");
+
+        gewinn = 0;
+        restart();
+    }
+
+    public void restart() {
+        played = false;
+        stopping.setText("Start");
+        betting.setDisable(true);
+        betValue.setDisable(true);
+        stopping.setDisable(false);
+        newCardButton.setDisable(true);
+        betted.setText("Betted:");
+        onWinLabel.setText("On win:");
     }
 
     @FXML
@@ -66,54 +142,189 @@ public class HelloController {
         Card neu = new Card();
         cards.add(neu);
         cardsCountedValue += neu.getValue();
+        String update = "";
+        for (Card c : cards) {
+            update += "[" + c.getName() + "]\n";
+        }
+        cardDeck.setText("Cards\n" + update);
+        countedCards.setText("Counted: " + cardsCountedValue);
         if (cardsCountedValue > 21) {
-            fail();
-        } else {
-            String update = "";
-            for (Card c : cards) {
-                update += "[" + c.getName() + "]";
+            if (!cheat) {
+                fail();
+            } else {
+                win();
             }
-            data.add(neu.getName());
-            cardOverview.setItems(data);
         }
     }
 
     @FXML
-    protected void start() {
-        Card onePlayer = new Card();
-        Card oneDealer = new Card();
-        Card twoPlayer = new Card();
-        Card twoDealer = new Card();
-
-        cardsCountedValue += onePlayer.getValue() + twoPlayer.getValue();
-        cardsCountedDealer += oneDealer.getValue() + twoDealer.getValue();
-
-        data.add(onePlayer.getName());
-        data.add(twoPlayer.getName());
-        cardOverview.setItems(data);
+    protected void restartPress() {
+        guthaben = 2000;
+        guthabenLabel.setText("Guthaben: " + guthaben + "€");
+        restartButton.setVisible(false);
+        cardDeckDealer.setText("[]");
+        profit.setText("[]");
+        stopping.setText("Stop");
+        betting.setDisable(false);
+        betValue.setDisable(false);
+        betValue.setEditable(true);
+        stopping.setDisable(true);
+        newCardButton.setDisable(true);
+        profit.setVisible(false);
+        played = true;
+        countedCardsBank.setText("Bank:");
+        mult.setText("Mult:");
+        restart();
     }
 
-    @FXML
-    protected void stopCardPull() {
-        if (cardsCountedValue > 21) {
-            fail();
+    public void ending() {
+        String update = "";
+        while (cardsCountedDealer < 17) {
+            Card newDealer = new Card();
+            cardsCountedDealer += newDealer.getValue();
+            cardsDealer.add(newDealer);
+            for (Card c : cardsDealer) {
+                update += c.getName() + "; ";
+            }
+        }
+        cardDeckDealer.setText("[ " + update.substring(0, update.length() - 2) + " ]");
+        countedCardsBank.setText("Bank: " + cardsCountedDealer);
+        if (cardsCountedDealer > 21) {
+            win();
+            return;
+        }
+        if (cardsCountedDealer >= cardsCountedValue) {
+            if (!cheat) {
+                fail();
+            } else {
+                win();
+            }
         } else {
             win();
         }
     }
 
     @FXML
+    protected void start() {
+        cards.clear();
+        cardsDealer.clear();
+        cardsCountedValue = 0;
+        cardsCountedDealer = 0;
+
+        Card onePlayer = new Card();
+        cards.add(onePlayer);
+        Card oneDealer = new Card();
+        cardsDealer.add(oneDealer);
+        Card twoPlayer = new Card();
+        cards.add(twoPlayer);
+        Card twoDealer = new Card();
+        cardsDealer.add(twoDealer);
+
+        cardsCountedValue += onePlayer.getValue() + twoPlayer.getValue();
+        cardsCountedDealer += oneDealer.getValue() + twoDealer.getValue();
+
+        String update = "";
+        for (Card c : cards) {
+            update += "[" + c.getName() + "]\n";
+        }
+        cardDeck.setText("Cards\n" + update);
+
+        countedCards.setText("Counted: " + cardsCountedValue);
+
+        stopping.setDisable(false);
+
+        newCardButton.setDisable(false);
+
+        betValue.setText("");
+
+        if (cardsCountedValue == 21) {
+            win();
+        }
+    }
+
+    @FXML
+    protected void stopCardPull() {
+        if (played == false) {
+            cardDeckDealer.setText("[]");
+            stopping.setText("Stop");
+            betting.setDisable(false);
+            betValue.setDisable(false);
+            betValue.setEditable(true);
+            stopping.setDisable(true);
+            newCardButton.setDisable(true);
+            played = true;
+            countedCardsBank.setText("Bank:");
+            mult.setText("Mult:");
+            return;
+        }
+        if (cardsCountedDealer < 17) {
+            ending();
+        } else if (cardsCountedValue <= cardsCountedDealer) {
+            if (!cheat) {
+                fail();
+            } else {
+                win();
+            }
+        } else win();
+    }
+
+    public void pressEnter(KeyEvent e) {
+        if (e.getCode().equals(KeyCode.ENTER)) {
+            betMoney();
+        }
+    }
+
+    public static int betInput;
+
+    @FXML
     protected void betMoney() {
-        if (Integer.parseInt(betValue.getText()) > guthaben) {
-            betted.setText("Error");
-            return;
+        if (!cheat) {
+            betting.setText("Bet");
         }
-        if (Integer.parseInt(betValue.getText()) < 0) {
-            betted.setText("Error");
-            return;
+
+        try {
+            betInput = Integer.parseInt(betValue.getText());
+
+            if (betInput == -42) {
+                if (!cheat) {
+                    cheat = true;
+                    betting.setText("Cheat");
+                    return;
+                } else {
+                    cheat = false;
+                    betting.setText("Bet");
+                    return;
+                }
+            }
+
+            if (betInput > guthaben) {
+                betting.setText("Error1");
+                return;
+            }
+            if (betInput < 0) {
+                betting.setText("Error2");
+                return;
+            }
+            multiplier = (int) (Math.random() * 10 + 2);
+            superWin = (int) (Math.random() * 100 + 1);
+            if (superWin == 42) {
+                multiplier = 100;
+            }
+            guthaben -= betInput;
+            gewinn = betInput * multiplier;
+            betted.setText("Betted: [ " + betInput + " ]€");
+            onWinLabel.setText("On win: [ " + gewinn + " ]€");
+            betting.setDisable(true);
+            betValue.setEditable(false);
+            guthabenLabel.setText("Guthaben: " + guthaben + "€");
+            mult.setText("Mult: [" + multiplier + "x]");
+            start();
         }
-        guthaben -= Integer.parseInt(betValue.getText());
-        betted.setText("Betted: [ " + Integer.parseInt(betValue.getText()) + " ]");
-        onWinLabel.setText("On win: [ " + Integer.parseInt(betValue.getText()) * 2 + " ]");
+        catch (NumberFormatException f) {
+            betting.setText("Error3");
+        }
+        catch (Exception g) {
+            betting.setText("Error4");
+        }
     }
 }
